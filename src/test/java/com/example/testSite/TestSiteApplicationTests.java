e9c6e4c5c29ae337124e99b1e533e26e8c7317de
Dmitriy Breyfogel <breyfogeld@bk.ru>
2025-04-11 20:10:41 +0300

    Стартовая версия. Все зависимости в pom.xml подключены, ошибок нет. Никакого интерйфейса, никакой реализации. Чтобы подтянуть все зависимости: ПКМ на pom.xml -> Maven -> Sync Project. Начало разработки сайта

---
+++ src/test/java/com/example/testSite/TestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
